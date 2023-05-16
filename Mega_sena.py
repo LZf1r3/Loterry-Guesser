8fb09b0c0baf390b30aaf7f013a740366a2628b1
LZf1r3 <lucas.mfaiad@gmail.com>
2023-05-16 16:13:23 -0700

    Lottery Guesser (Brazil)

--- Mega_sena.py
+++ Mega_sena.py
@@ -3,14 +3,14 @@
 import random
 def Mega():
     print("Bem vindo ao adivinhador de Mega sena LZ!")
-    number_rows = int(
+    numeros_tentados = int(
         input("Por gentileza digite a quantidade de numeros que deseja apostar:\n"))
-    number_of_lottery = int(input("Por gentileza digite a quantidade de cartelas:\n"))
+    quantidade_de_loterias = int(input("Por gentileza digite a quantidade de cartelas:\n"))
 
-    while number_of_lottery > 0:
-        rolls = random.sample(range(1, 60), number_rows)
+    while quantidade_de_loterias > 0:
+        rolls = random.sample(range(1, 60), numeros_tentados)
         print(rolls)
-        number_of_lottery -= 1
+        quantidade_de_loterias -= 1
 
 if __name__ == "__main__":
     Mega()
